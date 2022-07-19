9422013fc4f35e29e86fce59c5e116a903bc54b4
selma <sunal.32@hotmail.com>
2022-07-19 07:00:03 +0100

    ready for first test case

---
+++ src/test/java/com/cydeo/day2/SpartanGetRequests.java
@@ -0,0 +1,11 @@
+package com.cydeo.day2;
+
+public class SpartanGetRequests {
+
+    String url ="http://54.211.143.220:800/api/spartans";
+    //  Given  Accept type application/json
+    //  When user send GET request to api/spartans end point
+    //  Then response content type must be application/json
+    //  And response body should include spartan result
+
+}
