1571e20c48a16bc56f95f2c2d6bec59feff3c9be
selma <sunal.32@hotmail.com>
2022-07-19 17:42:39 +0100

    Task added to HR

--- src/test/java/com/cydeo/day2/HrGetRequests.java
+++ src/test/java/com/cydeo/day2/HrGetRequests.java
@@ -1,7 +1,10 @@
 package com.cydeo.day2;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class HrGetRequests {
 
@@ -10,6 +13,26 @@ public class HrGetRequests {
     @BeforeAll
     public static void init(){
 
+        //Save BaseURI inside this variable so that we don't need to type each http method.
         RestAssured.baseURI = "http://54.211.143.220:1000/ords/hr";
     }
+
+    @DisplayName("GET request to /regions")
+    @Test
+    public void test1(){
+        Response response = RestAssured.get("/regions");
+
+        //print the status code
+        System.out.println(response.statusCode());
+    }
+
+    /*
+    Given accept type is application/json
+    When user sends GET request the /regions/2
+    Then response status code must be 200
+    and content type equals to application/json format
+    and response body contains Americas
+     */
+
+    RestAssured.
 }
