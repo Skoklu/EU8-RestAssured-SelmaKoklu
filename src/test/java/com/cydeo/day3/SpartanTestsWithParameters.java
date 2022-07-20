d3cced4eb38458d4d91eb01b190179804f9f4a17
selma <sunal.32@hotmail.com>
2022-07-20 09:26:44 +0100

    Spartan Test with papaneters

--- src/test/java/com/cydeo/day3/SpartanTestsWithParameters.java
+++ src/test/java/com/cydeo/day3/SpartanTestsWithParameters.java
@@ -14,4 +14,13 @@ public class SpartanTestsWithParameters {
         //save baseUrl inside this variable so that we don't need to type each http method
         baseURI = "http://54.211.143.220:8000";
     }
+
+    /*
+    Given accept type is json
+    And ID parameter value is 5
+    When user send GET request to /api/spartans/{id}
+    Then response status code should be 200
+    And response content type: application/json
+    And "Blythe" should be in response payload
+     */
 }
