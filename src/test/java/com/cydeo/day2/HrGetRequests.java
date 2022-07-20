7e7885533e0133a85e3c694107bee64134dabe59
selma <sunal.32@hotmail.com>
2022-07-20 01:00:33 +0100

    Static import implemented

--- src/test/java/com/cydeo/day2/HrGetRequests.java
+++ src/test/java/com/cydeo/day2/HrGetRequests.java
@@ -1,10 +1,15 @@
 package com.cydeo.day2;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HrGetRequests {
 
@@ -24,9 +29,10 @@ public class HrGetRequests {
 
         //print the status code
         System.out.println(response.statusCode());
-    }
 
-    /*
+
+    }
+   /*
     Given accept type is application/json
     When user sends GET request the /regions/2
     Then response status code must be 200
@@ -34,5 +40,23 @@ public class HrGetRequests {
     and response body contains Americas
      */
 
-    RestAssured.
+    @DisplayName("GET request to /regions/2")
+    @Test
+    public void test2(){
+        Response response =given().accept(ContentType.JSON)
+                .when().get("/regions/2");
+
+        //verify status code
+        assertEquals(200, response.statusCode());
+
+        //verify content type
+        assertEquals("application/json",response.contentType());
+
+        //verify body contains Americas
+        response.prettyPrint();
+        assertEquals(response.body().asString().contains("Americas"),true);
+    }
+
+
+
 }
