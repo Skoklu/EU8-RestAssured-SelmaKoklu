dc9d04dc0918d8f76a62dec39cb495a90971ab61
selma <sunal.32@hotmail.com>
2022-07-20 09:42:19 +0100

    Day3 Spartan Test with parameters

--- src/test/java/com/cydeo/day3/SpartanTestsWithParameters.java
+++ src/test/java/com/cydeo/day3/SpartanTestsWithParameters.java
@@ -1,6 +1,10 @@
 package com.cydeo.day3;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,4 +27,20 @@ public class SpartanTestsWithParameters {
     And response content type: application/json
     And "Blythe" should be in response payload
      */
+
+    @DisplayName("GET request to /api/spartans/{id} with ID 5")
+    @Test
+    public void test1(){
+
+        given().
+                accept(ContentType.JSON)
+                        .and()
+                            .pathParam("id",5)
+                .when()
+                .get("/api/spartans/{id}");
+
+
+
+
+    }
 }
