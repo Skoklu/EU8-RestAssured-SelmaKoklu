4ef925cd9ad078380908c0ba30264206657a7533
selma <sunal.32@hotmail.com>
2022-07-20 10:15:51 +0100

    Day3 pathParam test

--- src/test/java/com/cydeo/day3/SpartanTestsWithParameters.java
+++ src/test/java/com/cydeo/day3/SpartanTestsWithParameters.java
@@ -51,4 +51,43 @@ public class SpartanTestsWithParameters {
 
 
     }
+    /*
+    Given accept type is json
+    And ID parameter value is 500
+    When user sends GET request to /api/spartans/{id}
+    Then response status code should be 404
+    And response content type: application/json
+    And "Not Found" message should be in response payload
+     */
+
+    @DisplayName("GET request /api/spartans/{id} Negative Test")
+    @Test
+    public void test2(){
+        Response response = given().accept(ContentType.JSON)
+                .pathParam("id",500)
+                .when()
+                .get("/api/spartans/{id}");
+
+        assertEquals(404,response.statusCode());
+
+        assertEquals("application/json", response.contentType());
+
+        assertTrue(response.body().asString().contains("Not Found"));
+
+        //verify status code
+        assertEquals(404,response.statusCode());
+
+        //verify content type
+        assertEquals("application/json",response.contentType());
+
+        //verify Blythe is in the json payload
+        assertTrue(response.body().asString().contains("Not Found"));
+
+        response.prettyPrint();
+    }
+
+
+
+
+
 }
