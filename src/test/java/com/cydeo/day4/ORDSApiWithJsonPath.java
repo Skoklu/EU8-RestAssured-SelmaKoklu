39a8aa4c433c0751fa40b3eb5171ae5e85d58ef9
selma <sunal.32@hotmail.com>
2022-07-21 09:42:41 +0100

    Day4: ORDSApiWithJsonPath

---
+++ src/test/java/com/cydeo/day4/ORDSApiWithJsonPath.java
@@ -0,0 +1,18 @@
+package com.cydeo.day4;
+
+import com.cydeo.utilities.HrTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ORDSApiWithJsonPath extends HrTestBase {
+
+
+    @DisplayName("GET request to countries")
+    @Test
+    public void test1(){
+
+    }
+}
