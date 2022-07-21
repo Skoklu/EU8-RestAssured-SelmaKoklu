320c4c7bfe720eac76d3bdf58c1c4232e7b374c6
selma <sunal.32@hotmail.com>
2022-07-21 10:01:39 +0100

    Day4: ORDSApiWithJsonPath

--- src/test/java/com/cydeo/day4/ORDSApiWithJsonPath.java
+++ src/test/java/com/cydeo/day4/ORDSApiWithJsonPath.java
@@ -1,8 +1,12 @@
 package com.cydeo.day4;
 
 import com.cydeo.utilities.HrTestBase;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,6 +17,24 @@ public class ORDSApiWithJsonPath extends HrTestBase {
     @DisplayName("GET request to countries")
     @Test
     public void test1(){
+
+        Response response = get("/countries");
+
+        //get the second country name with JsonPath
+
+
+        //to use JsonPath we assign response to JsonPath
+
+        JsonPath jsonPath = response.jsonPath();
+
+        String secondCountryName = jsonPath.getString("items[1].country_name");
+
+        System.out.println("secondCountryName = " + secondCountryName);
+
+        //get all country ids
+
+        List<String> allCountryIDs = jsonPath.getList("items.country_id");
+        System.out.println("allCountryIDs = " + allCountryIDs);
 
     }
 }
