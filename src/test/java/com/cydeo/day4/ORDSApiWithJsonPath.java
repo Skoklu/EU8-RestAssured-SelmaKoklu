5bb14cf52de198442ad422d56c04df0640ddd671
selma <sunal.32@hotmail.com>
2022-07-22 06:47:26 +0100

    ORDSApiWithJsonPath

--- src/test/java/com/cydeo/day4/ORDSApiWithJsonPath.java
+++ src/test/java/com/cydeo/day4/ORDSApiWithJsonPath.java
@@ -40,7 +40,8 @@ public class ORDSApiWithJsonPath extends HrTestBase {
 
         //get all country names where their region id is equal to 2
 
-        List<String> countryNameWithRegionID2 = jsonPath.getList("items.findAll {it.region_id==2}.country_name");
+        List<String> countryNameWithRegionID2 =
+                jsonPath.getList("items.findAll {it.region_id==2}.country_name");
         System.out.println("countryNameWithRegionID2 = " + countryNameWithRegionID2);
 
     }
@@ -48,6 +49,37 @@ public class ORDSApiWithJsonPath extends HrTestBase {
     @DisplayName("GET request /employees with query param")
     @Test
      public void test2(){
+
+        //We added query param to get 107 employees
+        Response response =given().queryParam("limit",107)
+                .when()
+                .get("/employees");
+
+
+        JsonPath jsonPath = response.jsonPath();
+
+        List<String> employeeITProg = jsonPath.getList("items.findAll{it.job_id == \"IT_PROG\"}.email");
+        System.out.println("employeeITProg = " + employeeITProg);
+
+
+        //get me first name of employees who is making more than 10000
+
+        List<String> EmployeesSalary =
+                jsonPath.getList("items.findAll {it.salary >10000}.first_name");
+
+        System.out.println("EmployeesSalary = " + EmployeesSalary);
+
+
+        //get me first name who is making max salary
+        //Json format
+        String firstNameMaxSalary1 = jsonPath.getString("items.max{it.salary}.first_name");
+        System.out.println(firstNameMaxSalary1);  //execution: Steven
+
+        //Response format
+        String firstNameMaxSalary2 = response.path("items.max {it.salary}.first_name");
+        System.out.println(firstNameMaxSalary2);   //execution: Steven
+
+
 
     }
 
