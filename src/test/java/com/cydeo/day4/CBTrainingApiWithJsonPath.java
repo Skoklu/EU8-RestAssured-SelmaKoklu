fc2533bfdf1352ad0770213f8adfdc694a3e7bf0
selma <sunal.32@hotmail.com>
2022-07-25 04:54:22 +0100

    Day4: CBTrainingApiWithJsonPath

--- src/test/java/com/cydeo/day4/CBTrainingApiWithJsonPath.java
+++ src/test/java/com/cydeo/day4/CBTrainingApiWithJsonPath.java
@@ -15,30 +15,29 @@ public class CBTrainingApiWithJsonPath {
         //Save BaseURI inside this variable so that we don't need to type each http method.
         baseURI = "http://api.cybertektraining.com";
 
-
-
-
     }
 
+
+    /* -send a get request to student id 23401 as a path parameter and accept header application/json
+       -verify status code = 200/ content type  = application/json;charset=UTF-8/ Content-Encoding = gzip
+       -Verify date header exist
+       -Assert that:
+       1. first_name: Vera
+       2. batch: 14
+       3. section: 12
+       4. emailAddress: aaa@gmail.com
+       5. company name: Cybertek
+       6. state: IL
+       7. zipCode: 60606
+
+       using jsonPath
+
+
+        */
     @DisplayName("GET request to individual student")
     @Test
     public void test1(){
 
-        /* -send a get request to student id 23401 as a path parameter and accept header application/json
-        -verify status code = 200/ content type  = application/json;charset=UTF-8/ Content-Encoding = gzip
-        -Verify date header exist
-        -Assert that:
-        1. first_name: Vera
-        2. batch: 14
-        3. section: 12
-        4. emailAddress: aaa@gmail.com
-        5. company name: Cybertek
-        6. state: IL
-        7. zipCode: 60606
 
-        using jsonPath
-
-
-         */
     }
 }
