b66b1886a8ad578ffabc3736c9f93906aa729375
selma <sunal.32@hotmail.com>
2022-07-25 04:55:25 +0100

    Day5: HamcrestMatchersIntro.java

---
+++ src/test/java/com/cydeo/day5/HamcrestMatchersIntro.java
@@ -0,0 +1,12 @@
+package com.cydeo.day5;
+
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
+
+public class HamcrestMatchersIntro {
+
+    @Test
+    public void simpleTest1(){
+
+    }
+}
