b9f3942c8961240911e2ff081e60ffbe2d22929d
selma <sunal.32@hotmail.com>
2022-07-26 06:12:08 +0100

    Day5: HamcrestMatchersApiTest

---
+++ src/test/java/com/cydeo/day5/HamcrestMatchersApiTest.java
@@ -0,0 +1,18 @@
+package com.cydeo.day5;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static  org.hamcrest.Matchers.*;
+
+public class HamcrestMatchersApiTest {
+
+    @DisplayName("OneSpartan with Hamcrest and Chaining")
+    @Test
+    public void test1(){
+
+    }
+}
