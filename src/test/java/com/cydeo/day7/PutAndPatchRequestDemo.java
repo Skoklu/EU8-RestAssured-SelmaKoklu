8e0232dfc35223cfa952454c3b1b10c5e67a64fb
selma <sunal.32@hotmail.com>
2022-07-30 12:45:41 +0100

    Day7: PutAndPatchRequestDemo.java

---
+++ src/test/java/com/cydeo/day7/PutAndPatchRequestDemo.java
@@ -0,0 +1,13 @@
+package com.cydeo.day7;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PutAndPatchRequestDemo {
+
+    @DisplayName("PUT request to one spartan for update with map")
+    @Test
+    public void PUTRequest(){
+
+    }
+}
