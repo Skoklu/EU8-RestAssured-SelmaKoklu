4640512933fde989d52bfd0b4d964a495599938a
selma <sunal.32@hotmail.com>
2022-07-30 22:14:43 +0100

    Day8: SpartanWithAuthTests.java

---
+++ src/test/java/com/cydeo/utilities/SpartanAuthTestBase.java
@@ -0,0 +1,19 @@
+package com.cydeo.utilities;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.baseURI;
+
+public abstract class SpartanAuthTestBase {
+
+    @BeforeAll
+    public static void init(){
+
+        //Save BaseURI inside this variable so that we don't need to type each http method.
+        baseURI = "http://54.211.143.220:7000";
+
+
+
+    }
+}
